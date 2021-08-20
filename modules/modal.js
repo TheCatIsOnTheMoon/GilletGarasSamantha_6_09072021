037f0fb6f25b3ba84cbb49e638a5dd6e6ad0c5fc
Sam Gillet-Garas <sgilletgaras@gmail.com>
2021-08-20 16:18:14 +0200

    close modal with escape key

--- modules/modal.js
+++ modules/modal.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // DOM Elements --------------------------------------------------------
 const modalbg = document.querySelector(".contact-form-bground");
 const form = document.getElementById("form");
@@ -15,3 +17,19 @@ document
 document.getElementById("closeBtn").addEventListener("click", function () {
   modalbg.style.display = "none";
 });
+
+// escape key to close modal
+document.onkeydown = function(event) {
+  event = event || window.event;
+  if (event.key == 'Escape' ) {
+    modalbg.style.display = "none";
+  }
+};
+
+// 'keyCode' is deprecated.
+
+// keyName == 'escape'
+// keyCode == 27
+// KeyboardEvent: key='Escape' | code='Escape'
+
+// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
