7b160e022c5001cf5f544c81686b2ec7c808bf79
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2021-09-27 11:51:32 +0200

    sort issue in chrome fixed

--- jsmodules/sortgallery.js
+++ jsmodules/sortgallery.js
@@ -2,40 +2,56 @@ import {displayGallery} from "./gallery.js";
 
 export function sortGallery(galleryData) {
 
+    // REF : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
     let sortedData = "";
 
     document.getElementById("dropdown-menu-popularity").addEventListener("click", function () {
 
-        console.log("sort by popularity")
+        //sortedData = galleryData.sort((a, b) => a.likes < b.likes);
+        //this way was working perfectly on firefox but not on chrome, so more consistent way describe in :
+        //https://stackoverflow.com/questions/1969145/sorting-javascript-array-with-chrome
 
-        sortedData = galleryData.sort((a, b) => a.likes < b.likes);
-
-        console.log(sortedData)
+        sortedData = galleryData.sort(function( a , b ){
+            var result = a.likes == b.likes ? 0 : a.likes > b.likes ? -1 : 1
+            if(result === 0)
+            {
+            }
+            return result ;
+        });
 
         displayGallery(sortedData)
     });
 
     document.getElementById("dropdown-menu-date").addEventListener("click", function () {
 
-        console.log("sort by date")
+        //sortedData = galleryData.sort((a, b) => a.date > b.date);
+        //idem than prec
 
-        sortedData = galleryData.sort((a, b) => a.date > b.date);
-
-        console.log(sortedData)
+        sortedData = galleryData.sort(function( a , b ){
+            var result = a.date == b.date ? 0 : a.date < b.date ? -1 : 1
+            if(result === 0)
+            {
+            }
+            return result ;
+        });
 
         displayGallery(sortedData)
     });
 
     document.getElementById("dropdown-menu-title").addEventListener("click", function () {
 
-        console.log("sort by title")
+        //sortedData = galleryData.sort((a, b) => a.title > b.title);
+        //idem than prec
 
-        sortedData = galleryData.sort((a, b) => a.title > b.title);
-
-        console.log(sortedData)
+        sortedData = galleryData.sort(function( a , b ){
+            var result = a.title == b.title ? 0 : a.title < b.title ? -1 : 1
+            if(result === 0)
+            {
+            }
+            return result ;
+        });
         
         displayGallery(sortedData)
     });
 }
-
-// REF : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
