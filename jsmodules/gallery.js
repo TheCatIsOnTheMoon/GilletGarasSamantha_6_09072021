6f2649c058b5571e224b0d8a3e6a905889616d3e
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2021-10-02 09:27:09 +0200

    alt correction

--- jsmodules/gallery.js
+++ jsmodules/gallery.js
@@ -8,7 +8,7 @@ export function displayGallery(galleryData) {
             photographeGalleryDOM += `
                 
                     <figure class="gallery-card gallery-photo">
-                        <img src="img/${media.photographerId}/${media.image}" class="lightbox-link" alt="photo nommée: ${media.alt}" id="${media.id}" tabindex="0"/>
+                        <img src="img/${media.photographerId}/${media.image}" class="lightbox-link" alt="${media.alt}" id="${media.id}" tabindex="0"/>
                         <figcaption class="gallery-photo-caption">
                             <h3 class="photo-caption-title">${media.title}</h3>
                             <div class="photo-caption-likes">
@@ -24,8 +24,8 @@ export function displayGallery(galleryData) {
             photographeGalleryDOM += `
 
                     <figure class="gallery-card gallery-photo" id="${media.id}">
-                        <video class="lightbox-link" src="img/${media.photographerId}/${media.video}" alt="vidéo nommée: ${media.alt}" id="${media.id}" tabindex="0">
-                            <source src="img/${media.photographerId}/${media.video}" type="video/mp4" alt="vidéo nommée: ${media.alt}">
+                        <video class="lightbox-link" src="img/${media.photographerId}/${media.video}" alt="${media.alt}" id="${media.id}" tabindex="0">
+                            <source src="img/${media.photographerId}/${media.video}" type="video/mp4" alt="${media.alt}">
                             Your browser does not support the video tag.
                         </video> 
                         <figcaption class="gallery-photo-caption">
